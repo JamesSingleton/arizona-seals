0c9288feace969a23b50fb56b262a125cc5645a5
James Singleton <jamesrsingleton1@gmail.com>
2025-02-17 17:02:57 -0700

    chore: comment out studioHost

--- apps/studio/sanity.cli.ts
+++ apps/studio/sanity.cli.ts
@@ -2,16 +2,16 @@ import { defineCliConfig } from "sanity/cli";
 
 const projectId = process.env.SANITY_STUDIO_PROJECT_ID;
 const dataset = process.env.SANITY_STUDIO_DATASET;
-const host = process.env.HOST_NAME;
+// const host = process.env.HOST_NAME;
 
 export default defineCliConfig({
   api: {
     projectId: projectId,
     dataset: dataset,
   },
-  studioHost:
-    host && host !== "main"
-      ? `${host}-turbo-start-sanity`
-      : "turbo-start-sanity",
+  // studioHost:
+  //   host && host !== "main"
+  //     ? `${host}-turbo-start-sanity`
+  //     : "turbo-start-sanity",
   autoUpdates: false,
 });
